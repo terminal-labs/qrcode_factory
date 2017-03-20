20bd3696b830e04c46c68005cfdaca49ab02b8ec
Joseph Nix <nixjdm@terminallabs.com>
2017-03-19 23:19:00 -0500

    Adding protections against misuse of methods. Consolidating some methods.

--- qrfactory.py
+++ qrfactory.py
@@ -10,14 +10,13 @@
 # does not handle all ways of specifying image sizes.
 #########################################
 import io
-import StringIO
 
 import segno
 import svgutils.transform as sg
 import svgwrite ##TODO use this to replace svgutils
 
 class QRFactory:
-    def __init__(self, logo, to_encode, module_color="#000000", background_color="#FFFFFF", scale_factor=10, outfile=None):
+    def __init__(self, logo=None, to_encode=None, module_color="#000000", background_color="#FFFFFF", scale_factor=10, outfile=None):
         '''Create a qrcode with a logo.
 
         Encode data  as an svg and overlay a logo with a matte backdrop on
@@ -44,32 +43,29 @@ class QRFactory:
         self.background_color = background_color
         self.scale_factor = scale_factor
 
-        self.input_for_encoding(to_encode)
-        self.base_qr_code()
-        self.input_logo(logo)
-        self.config_logo()
-        self.create_plots()
-        self.output_qr()
+        if to_encode is not None: # Make base qr code
+            self.base_qr_code(to_encode)
 
-    def input_for_encoding(self, to_encode):
+        if logo is not None and to_encode is not None: # put logo on qrcode
+            self.logo(logo)
+            self.create_plots()
+
+    def base_qr_code(self, to_encode):
         ## Create base QR Code
         self.QRsvg = io.BytesIO()
         qr = segno.make(to_encode, micro=False, error='H')
+        # saving base qr code to StringIO buffer
         qr.save(self.QRsvg, color=self.module_color, background=self.background_color, kind='svg')
-        # outputting base qr code to StringIO buffer
-    def base_qr_code(self):
         self.fig_qr = sg.fromstring(self.QRsvg.getvalue())
         self.qr_size = float(self.fig_qr.get_size()[0]) # only grab first size because it's a square
         self.middle = (self.qr_size*self.scale_factor)/2 # typically not an integer
 
-    def input_logo(self,logo):
-        ## Load image to embed
-        self.fig_logo = sg.fromstring(logo)
-
-    def config_logo(self):
+    def logo(self, logo):
         ## This method will create the small background image that goes behind the logo, effectilly a matte. We needed it for our logo
         ## But it may not be necessary for MVP of site. Something we could add later as it is probably one of the trickyer things to generalize.
         # TODO: Following line needs to be more robust for arbitrary SVGs.
+        ## Load image to embed
+        self.fig_logo = sg.fromstring(logo)
         self.logo_size = float(self.fig_logo.root.get('viewBox').split()[2]) # only grab first size because it's a square
 
         ## Create embedded image's solid background. It will provide a 1 module ("pixel") wide margin in all directions.
@@ -78,29 +74,33 @@ class QRFactory:
         fig_background.add(fig_background.rect(insert=(0,0), size=(self.logo_box_size, self.logo_box_size), fill=self.module_color))
         self.fig_background = sg.fromstring(fig_background.tostring()) # patch data from svgwrite to svgutils
 
-    def create_plots(self):
+    def create_plots(self): #TODO: Not sure this method and output_qr should be separate. Consider condsolidating.
         ### Creating plots to be combined into final SVG
         ## Create QR code plot
         self.plot_qr = self.fig_qr.getroot()
         self.plot_qr.moveto(0, 0, scale=self.scale_factor)
 
         ## Create background plot
-        # Create a solid background behind the logo, extending such that there
-        # is one whole pixel of background in the margin around the logo
-        self.plot_background = self.fig_background.getroot()
-        background_translation = self.middle - self.logo_box_size/2 # center the background
-        self.plot_background.moveto(background_translation, background_translation)
+        if (self.fig_logo and self.fig_background): # if no logo is set, don't embed one (or it's background)
+            # Create a solid background behind the logo, extending such that there
+            # is one whole pixel of background in the margin around the logo
+            self.plot_background = self.fig_background.getroot()
+            background_translation = self.middle - self.logo_box_size/2 # center the background
+            self.plot_background.moveto(background_translation, background_translation)
 
-        ## Create logo plot
-        self.plot_logo = self.fig_logo.getroot()
-        logo_translation = self.middle - 7*self.scale_factor/2 # center the logo
-        # Scale to fit logo in a 7 module wide box in the center.
-        self.plot_logo.moveto(logo_translation, logo_translation, scale=7*self.scale_factor/self.logo_size)
+            ## Create logo plot
+            self.plot_logo = self.fig_logo.getroot()
+            logo_translation = self.middle - 7*self.scale_factor/2 # center the logo
+            # Scale to fit logo in a 7 module wide box in the center.
+            self.plot_logo.moveto(logo_translation, logo_translation, scale=7*self.scale_factor/self.logo_size)
 
     def output_qr(self):
         ## Combine plots into single SVG
         fig = sg.SVGFigure(self.qr_size*self.scale_factor, self.qr_size*self.scale_factor)
-        fig.append([self.plot_qr, self.plot_background, self.plot_logo]) # Order Matters. First is lowest z-index.
+        if (self.plot_logo and self.plot_background): # Logo and background are set
+            fig.append([self.plot_qr, self.plot_background, self.plot_logo]) # Order Matters. First is lowest z-index.
+        else:
+            fig.append(self.plot_qr) # Just a qr code
         return fig.to_str() # Can only save to a file or be cast into a string
 
 
